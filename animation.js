75458ca61e7fe4a8efa31a46f36d60dd104caebc
gaetanslrt <gaetan.slt@gmail.com>
2025-02-25 12:58:34 +0100

    styling

--- animation.js
+++ animation.js
@@ -4,6 +4,8 @@
         const startDelay = 1350; 
         let i = 0;
         const title = document.querySelector(".slogan");
+        const element = document.getElementById("show");
+        element.style.opacity = 0;
 
         function type() {
             if (i < text.length) {
@@ -15,6 +17,22 @@
                     title.classList.add("done");
                 }, startDelay); 
             }
+        }
+
+        function show() {
+            let opacity = 0;
+            const interval = 5;
+            const increment = 0.05;
+
+            function fadeIn() {
+                if (opacity < 1) {
+                    opacity += increment;
+                    element.style.opacity = opacity;
+                    setTimeout(fadeIn, interval);
+                }
+            }
+
+            setTimeout(fadeIn, 4500);
         }
 
         
